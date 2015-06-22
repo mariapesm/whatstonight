49b6b07e06eead977821658fbe143d608bbc0153
Colin Hill <Colin@HP-PrintshareTM.local>
2015-06-22 10:56:11 -0500

    initial commit

---
+++ client/app/admin/admin.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('whatstonightApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/admin', {
+        templateUrl: 'app/admin/admin.html',
+        controller: 'AdminCtrl'
+      });
+  });
---
+++ client/app/main/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('whatstonightApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
