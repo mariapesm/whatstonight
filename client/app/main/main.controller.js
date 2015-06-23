42f37be4d885e949bf7fa247562b5212b3b31b81
Colin Hill <Colin@HP-PrintshareTM.local>
2015-06-23 18:19:58 -0500

    purpleeeee

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -4,14 +4,18 @@ angular.module('whatstonightApp')
   .controller('MainCtrl', function ($scope, $http, User, Auth) {
     $scope.awesomeThings = [];
 
-    $scope.location = "77006";
 
-    $scope.searchBars = function() {  
+    $scope.searchBars = function() {
       $http.get('/api/bars/start/' + $scope.location).success(function(Bars) {
         $scope.bars = Bars;
       })
     }
 
-
+    $scope.going = function(bar) {
+      bar.attendees.push(getCurrentUser()._id);
+      $http.post('/api/bars/',bar).success(function(Bars) {
+        $scope.bars = Bars;
+      })
+    }
 
   });
