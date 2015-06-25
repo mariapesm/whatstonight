8a3fa8645165c83f96b47384bcd778d245ac5e9d
Colin Hill <Colin@HP-PrintshareTM.local>
2015-06-24 22:47:00 -0500

    persistent locaiton?

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -4,6 +4,7 @@ angular.module('whatstonightApp')
   .controller('MainCtrl', function ($scope, $http, User, Auth) {
     $scope.awesomeThings = [];
     $scope.getCurrentUser = Auth.getCurrentUser;
+    $scope.isLoggedIn = Auth.isLoggedInAsync;
 
     console.log($scope.getCurrentUser());
     $scope.searchBars = function(location) {
@@ -32,7 +33,7 @@ angular.module('whatstonightApp')
       });
 
     }
-    if ($scope.getCurrentUser()._id !== undefined) {
+    if ($scope.isLoggedIn) {
       $scope.getCurrentUser().$promise.then(function(user){
         if (user.location!=="" && user.location !== undefined) {
             $scope.searchBars(user.location);
